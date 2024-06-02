36f57d1b5ea02213d23486aa14b61bfd28cf7169
miya Dong <miya.d@cxlavender.com.au>
2024-06-02 07:55:19 -0400

    fix importpath

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9c3930ab0e3ad944"; }
+/******/ 		__webpack_require__.h = function() { return "ce201d3ef7ccca63"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
